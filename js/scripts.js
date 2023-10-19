12f80ed7cb776e2d7acf15dee0a7f225609a08a8
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-19 13:34:24 -0700

    Finalize operator functions. Finalize calculation steps.
    Co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com>
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>
    Co-authored-by: Grant Abel <gtabel88@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,24 +17,28 @@ function divide(num1,num2) {
   return num1 / num2;
 }
 
-function calculate(num1,num2,operator) {
+function calculate(num1,num2) {
   if (document.querySelector("input[name='operator']:checked").value === "add") {
-    
+    return add(num1,num2);
   } else if (document.querySelector("input[name='operator']:checked").value === "subtract") {
-
+    return subtract(num1,num2);
   } else if (document.querySelector("input[name='operator']:checked").value === "multiply") {
-
+    return multiply(num1,num2);
   } else if (document.querySelector("input[name='operator']:checked").value === "divide") {
-
+    return divide(num1,num2);
   }
 }
 
-function getNumbers {
-  
-}
-
 window.addEventListener("load", function() {
-document.getElementById("calculator").addEventListener("sumbit", getNumbers)
+document.getElementById("calculator").addEventListener("submit", getNumbers)
 } )
 
 // UI Logic
+
+function getNumbers(event) {
+  event.preventDefault();
+  let num1 = parseInt(document.getElementById("inputBox1").value);
+  let num2 = parseInt(document.getElementById("inputBox2").value);
+  let result = calculate(num1,num2);
+  document.getElementById("result").innerText = result;
+}
