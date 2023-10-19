9f090f22521b96a2b251b4dfeeb0ff0806432010
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-19 12:02:27 -0700

    Add basic logic for operators and calculating. Add window EventListener onload to wait for submit button to be pressed to run getNumbers function
    Co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com>
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// Business Logic
+
+// Operators
+function add(num1,num2) {
+  return num1 + num2;
+}
+
+function subtract(num1,num2) {
+  return num1 - num2;
+}
+
+function multiply(num1,num2) {
+  return num1 * num2;
+}
+
+function divide(num1,num2) {
+  return num1 / num2;
+}
+
+function calculate(num1,num2,operator) {
+  if (document.querySelector("input[name='operator']:checked").value === "add") {
+    
+  } else if (document.querySelector("input[name='operator']:checked").value === "subtract") {
+
+  } else if (document.querySelector("input[name='operator']:checked").value === "multiply") {
+
+  } else if (document.querySelector("input[name='operator']:checked").value === "divide") {
+
+  }
+}
+
+function getNumbers {
+  
+}
+
+window.addEventListener("load", function() {
+document.getElementById("calculator").addEventListener("sumbit", getNumbers)
+} )
+
+// UI Logic
